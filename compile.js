6e15d25327a322097e26694ace8cf214e4ad86c5
skatz1990 <skatz1990@gmail.com>
2021-04-13 16:16:41 -0400

    added compile and deploy

---
+++ compile.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const lotteryPath = path.resolve(__dirname, 'contracts', 'Lottery.sol');
+const source = fs.readFileSync(lotteryPath, 'utf8');
+
+var input = {
+    language: 'Solidity',
+    sources: {
+        'Lottery.sol': {
+            content: source
+        }
+    },
+    settings: {
+        outputSelection: {
+            '*': {
+                '*': ['*']
+            }
+        }
+    }
+};
+
+var compiled = JSON.parse(solc.compile(JSON.stringify(input)));
+module.exports = compiled.contracts["lottery.sol"].lottery;
+console.log(compiled);
---
+++ deploy.js
@@ -0,0 +1,28 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { abi, evm } = require('./compile');
+
+const provider = new HDWalletProvider(
+    'YOUR PHRASE',
+    'rinkeby endpoint'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+
+    accounts = await web3.eth.getAccounts();
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(abi)
+        .deploy({
+            data: "0x" + evm.bytecode.object
+        })
+        .send({
+            from: accounts[0],
+            gas: '1000000'
+        });
+
+    console.log('Contract deployed to', result.options.address);
+};
+deploy();
