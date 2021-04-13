6140a94dd4348a32cc5ed7f9a50a9adeec87fea5
skatz1990 <skatz1990@gmail.com>
2021-04-13 11:12:18 -0400

    Fixed tests issue

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,7 +1,8 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 const { abi, evm } = require('../compile');
 
 let accounts;
@@ -10,16 +11,30 @@ let inbox;
 beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
 
-    inbox = await new web3.eth.Contract(abi, evm.bytecode, {
-        from: accounts[0], gas: '1000000'
-    });
-
-    //inbox.methods.setMessage("My message");
+    inbox = await new web3.eth.Contract(abi)
+        .deploy({
+            data: "0x" + evm.bytecode.object,
+            arguments: ["My message"]
+        })
+        .send({
+            from: accounts[0],
+            gas: '1000000'
+        });
 });
 
 describe('Inbox', () => {
-    it('sets the message', () => {
-        console.log(inbox.methods.getMessage().call());
-        assert(inbox.methods.getMessage() === "My message");
+    it('Deploys a contract', () => {
+        assert.ok(inbox.options.address);
     });
+
+    it('Has a default message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.strictEqual(message, "My message");
+    })
+
+    it('Can change the message', async () => {
+        await inbox.methods.setMessage('bye').send({ from: accounts[0] });
+        const message = await inbox.methods.message().call();
+        assert.strictEqual(message, 'bye');
+    })
 });
