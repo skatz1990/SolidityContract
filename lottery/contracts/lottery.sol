974390e3ee7d7d3743a67e73781b15a1ff0d0795
skatz1990 <skatz1990@gmail.com>
2021-04-13 16:12:06 -0400

    Added getPlayers

--- lottery/contracts/lottery.sol
+++ lottery/contracts/lottery.sol
@@ -32,4 +32,8 @@ contract Lottery {
         require(msg.sender == manager);
         _;
     }
+    
+    function getPlayers() public view returns (address payable[] memory) {
+        return players;
+    }
 }
