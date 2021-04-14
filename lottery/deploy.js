7df14ffa7e086cd02a052acb2c87e3ed00b64b08
skatz1990 <skatz1990@gmail.com>
2021-04-14 10:50:57 -0400

    fixed deploy.js issue

--- lottery/deploy.js
+++ lottery/deploy.js
@@ -1,11 +1,16 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
 const { abi, evm } = require('./compile');
+const mnemonicPhrase = '';
 
-const provider = new HDWalletProvider(
-    'YOUR PHRASE',
-    'rinkeby endpoint'
-);
+let provider = new HDWalletProvider({
+    mnemonic:{
+        phrase: mnemonicPhrase
+    },
+    providerOrUrl: '',
+    numberOfAddresses: 2,
+    addressIndex: 1
+});
 
 const web3 = new Web3(provider);
 
@@ -14,15 +19,16 @@ const deploy = async () => {
     accounts = await web3.eth.getAccounts();
     console.log('Attempting to deploy from account', accounts[0]);
 
-    const result = await new web3.eth.Contract(abi)
-        .deploy({
-            data: "0x" + evm.bytecode.object
-        })
-        .send({
-            from: accounts[0],
-            gas: '1000000'
-        });
+    try {
+        const result = await new web3.eth.Contract(abi)
+            .deploy({ data: "0x" + evm.bytecode.object })
+            .send({ gas: 1000000, from: accounts[0] });
 
-    console.log('Contract deployed to', result.options.address);
+        console.log(abi);
+        console.log('Contract deployed to', result.options.address);
+    } catch (err) {
+        console.log(err);
+    }
+
 };
 deploy();
